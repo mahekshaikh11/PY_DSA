d95d5cc2de3ab74e412fcc5e6fda336b433025f6
Mahek Shaikh <shaikhmahek6122@gmail.com>
2025-07-06 17:53:54 +0530

    code1.py

--- code1.py
+++ code1.py
@@ -1,6 +1,19 @@
-print("Mahek Shaikh")
+#how much max swap needed to sort array of lenght 6?
 
-a=2
-b=3
-c=a+b
-print(c)
+def bubble_sort(arr):
+    n=len(arr)
+    count =0
+
+    for i in range(n-1):
+        is_sorted = True
+        for j in range (n-i-1):
+            if (arr[j]>arr[j+1]):
+                is_sorted = False
+                count = count+1
+                #left more than right then swap
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+            if is_sorted:
+                break
+    return count
+
+print(bubble_sort([7,3,5,2,1]))            
